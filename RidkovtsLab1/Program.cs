4a181772b5d9752c7fa89ffd72f5a42f7c47c69d
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2026-09-18 01:00:45 +0300

    Create wallet generator

---
+++ RidkovtsLab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Nethereum.Signer;
+
+namespace RidkovtsLab1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string targetPrefix = "0x10"; // Число дня народження: 10
+
+        EthECKey ecKey;
+        string address;
+        int attempts = 0;
+
+        do
+        {
+            attempts++;
+            ecKey = EthECKey.GenerateKey();
+            address = ecKey.GetPublicAddress();
+        } 
+        while (!address.StartsWith(targetPrefix, StringComparison.OrdinalIgnoreCase));
+
+        Console.WriteLine($"\nЗнайдено гаманець після {attempts} спроб!");
+        Console.WriteLine($"Публічна адреса: {address}");
+        Console.WriteLine($"Приватний ключ:  0x{ecKey.GetPrivateKey()}");
+    }
+}
