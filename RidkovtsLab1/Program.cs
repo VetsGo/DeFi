741c9c1068aa7f3c6c10c2cfdd4a24bfe503a47a
Ridcovets_Sergo <springtrapukrainian@gmail.com>
2026-09-18 01:23:53 +0300

    Added balance display

--- RidkovtsLab1/Program.cs
+++ RidkovtsLab1/Program.cs
@@ -1,27 +1,23 @@
-﻿using Nethereum.Signer;
+﻿using Nethereum.Web3;
 
 namespace RidkovtsLab1;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        string targetPrefix = "0x10"; // Число дня народження: 10
+        string rpcUrl = "https://eth-sepolia.g.alchemy.com/v2/alch_ohPjfg0J-A371o42lIuj8";
+        var web3 = new Web3(rpcUrl);
+        
+        string myAddress = "0x1002b3FF6Fb2Cd594ea2Fa043Add0fce28e59856"; 
 
-        EthECKey ecKey;
-        string address;
-        int attempts = 0;
+        Console.WriteLine($"Retrieving the balance for an address: {myAddress}");
+        
+        var balanceWei = await web3.Eth.GetBalance.SendRequestAsync(myAddress);
+        
+        decimal balanceEther = Web3.Convert.FromWei(balanceWei.Value);
 
-        do
-        {
-            attempts++;
-            ecKey = EthECKey.GenerateKey();
-            address = ecKey.GetPublicAddress();
-        } 
-        while (!address.StartsWith(targetPrefix, StringComparison.OrdinalIgnoreCase));
-
-        Console.WriteLine($"\nWallet generated after {attempts} attempts!");
-        Console.WriteLine($"Public address: {address}");
-        Console.WriteLine($"Private key:  0x{ecKey.GetPrivateKey()}");
+        Console.WriteLine($"Balance in Wei:   {balanceWei.Value}");
+        Console.WriteLine($"Balance in Ether: {balanceEther} ETH");
     }
 }
